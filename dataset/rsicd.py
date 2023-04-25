487703b9dec15028602563bc15afdbcf15917ecd
Angelo Nazzaro <mutenssj@gmail.com>
2023-04-25 15:47:30 +0200

    create dataset package, reformat and clean code

--- dataset/rsicd.py
+++ dataset/rsicd.py
@@ -6,24 +6,21 @@ import torch
 import spacy
 import nlpaug.augmenter.word as naw
 from torch.utils.data import Dataset
-from torchvision import transforms as t
 from torchvision.io import read_image
+
+from .constants import DEFAULT_TRANSFORMS, IMAGE_FIELD, CAPTION_FIELD
 
 
 class RSICD(Dataset):
     """ Remote Sensing Image Captioning Dataset. """
 
-    def __init__(self, annotations_file, img_dir, img_transform=None, caption_transform=None,
-                 transform=None, target_transform=None):
+    def __init__(self, annotations_file, img_dir, img_transform=None, target_transform=None):
         """
             Arguments:
                 annotations_file (string): Path to the json file containing the annotations.
                 img_dir (string): Directory with all the images.
                 img_transform (callable, optional): Optional transform to be applied on an image in order to
                     perform data augmentation. If None, random transformations will be applied.
-                caption_transform (callable, optional): Optional transform to be applied on a caption in order to
-                    perform data augmentation. If None, synonym replacement will be applied.
-                transform (callable, optional): Optional transform to be applied on an image.
                 target_transform (callable, optional): Optional transform to be applied on a caption.
         """
         self.img_captions = pd.read_json(annotations_file)
@@ -32,12 +29,8 @@ class RSICD(Dataset):
         if img_transform:
             self.img_transform = img_transform
         else:
-            self.img_transform = t.RandomApply(torch.nn.ModuleList(
-                [t.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 3.85)),
-                 t.RandomRotation(degrees=(0, 180)), t.RandomHorizontalFlip(p=0.5), t.RandomVerticalFlip(p=0.5)]))
+            self.img_transform = DEFAULT_TRANSFORMS
 
-        self.caption_transform = caption_transform
-        self.transform = transform
         self.target_transform = target_transform
 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -62,20 +55,25 @@ class RSICD(Dataset):
         caption = sentence["raw"]
 
         image = self.img_transform(image)
-        if self.transform:
-            image = self.transform(image)
 
-        if self.caption_transform:
-            caption = self.caption_transform(caption)
-        else:
-            # apply synonym replacement
-            nlp = spacy.load("en_core_web_lg")
-            aug = naw.ContextualWordEmbsAug(model_path="bert-base-uncased", action="substitute", aug_min=0,
-                                            aug_max=len(sentence["tokens"]), stopwords=nlp.Defaults.stop_words,
-                                            device=self.device)
-            caption = aug.augment(caption)[0]
+        # apply synonym replacement
+        nlp = spacy.load("en_core_web_lg")
+        caption = augment_text(data=caption, model_path="bert-base-uncased", action="substitute", aug_min=0,
+                               aug_max=len(sentence["tokens"]), stopwords=nlp.Defaults.stop_words,
+                               device=self.device)[0]
+        # augment data with bert insert action
+        caption = augment_text(data=caption, model_path="bert-base-uncased", action="insert", aug_min=0,
+                               aug_max=4, stopwords=nlp.Defaults.stop_words,
+                               device=self.device)[0]
 
         if self.target_transform:
             caption = self.target_transform(caption)
 
-        return {"image": image, "caption": caption}
+        return {IMAGE_FIELD: image, CAPTION_FIELD: caption}
+
+
+def augment_text(data, model_path, action, aug_min, aug_max, stopwords, device='cpu'):
+    aug = naw.ContextualWordEmbsAug(model_path=model_path, action=action, aug_min=aug_min, aug_max=aug_max,
+                                    stopwords=stopwords, device=device)
+
+    return aug.augment(data)
