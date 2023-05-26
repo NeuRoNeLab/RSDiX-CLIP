18c084aa7b82064c05e184ca2b12499b9884964b
Angelo Nazzaro <angelonazzaro12@gmail.com>
2023-05-26 20:21:44 +0200

    reorganized arguments and set callbacks

--- train_finetune.py
+++ train_finetune.py
@@ -2,6 +2,7 @@ import torch
 
 from argparse import ArgumentParser
 from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from torchvision.models import resnet50, ResNet50_Weights
 from transformers import AutoTokenizer, AutoModel
 
@@ -17,12 +18,20 @@ def main(hparams):
     txt_encoder = AutoModel.from_pretrained("johngiorgi/declutr-sci-base")
 
     if hparams.batch_size < 1:
-        hparams.batch_size = 512
+        hparams.batch_size = 128
 
     seed_everything(hparams.seed, workers=True)
 
-    trainer = Trainer(precision=hparams.precision, accelerator=hparams.accelerator, devices=hparams.devices,
-                      max_epochs=hparams.max_epochs)
+    early_stopping = EarlyStopping(monitor=hparams.monitor_metric, mode=hparams.mode,
+                                   stopping_threshold=hparams.stopping_threshold,
+                                   divergence_threshold=hparams.divergence_threshold)
+    model_checkpoint = ModelCheckpoint(dirpath=hparams.dirpath, monitor=hparams.monitor_metric,
+                                       save_on_train_epoch_end=hparams.save_on_train_epoch_end, mode=hparams.mode,
+                                       filename=hparams.filename, save_top_k=hparams.save_top_k)
+
+    trainer = Trainer(precision=hparams.precision,
+                      accelerator=hparams.accelerator, devices=hparams.devices,
+                      max_epochs=hparams.max_epochs, callbacks=[early_stopping, model_checkpoint])
 
     dm = CaptioningDataModule(annotations_file=hparams.annotations_file, img_dir=hparams.img_dir,
                               custom_tokenizer=tokenizer, batch_size=hparams.batch_size,
@@ -38,22 +47,54 @@ def main(hparams):
 if __name__ == '__main__':
     parser = ArgumentParser()
 
+    parser.add_argument('--seed', type=int, default=42,
+                        help='the integer value seed for global random state in Lightning')
+
+    # model's arguments
     parser.add_argument('--model_name', type=str, default='RN50')
     parser.add_argument('--learning_rate', type=float, default=None)
     parser.add_argument('--kl_coeff', type=float, default=1.0)
-    parser.add_argument('--devices', type=int, default=-1)
     parser.add_argument('--warmup_steps', type=int, default=0)
+
+    # trainer's arguments
+    parser.add_argument('--devices', type=int, default=-1)
     parser.add_argument('--accelerator', type=str, default='cuda')
     parser.add_argument('--max_epochs', type=int, default=32)
     parser.add_argument('--precision', type=int, default=32)
+
+    # datamodule's arguments
     parser.add_argument('--annotations_file', type=str, required=True, help='annotation file of your training data')
     parser.add_argument('--img_dir', type=str, required=True, help='image directory of your training data')
     parser.add_argument('--train_split', type=float, default=80, help='percentage split for the training set')
     parser.add_argument('--val_split', type=float, default=10, help='percentage split for the validation set')
-    parser.add_argument('--batch_size', type=int, default=512, help='size of the batch')
     parser.add_argument('--num_workers', type=int, default=0, help='number of workers for the dataloaders')
     parser.add_argument('--shuffle', type=bool, default=False, help='whether to use shuffling during sampling')
-    parser.add_argument('--seed', type=int, default=42, help='random seed')
+
+    # early stopping arguments
+    parser.add_argument('--patience', type=int, default=3,
+                        help='number of checks with no improvement after which training will be stopped')
+    parser.add_argument('--stopping_threshold', type=float, default=None,
+                        help='Stop training immediately once the monitored quantity reaches this threshold')
+    parser.add_argument('--divergence_threshold', type=float, default=None,
+                        help='Stop training as soon as the monitored quantity becomes worse than this threshold')
+
+    # checkpointing arguments
+    parser.add_argument('--dirpath', type=str, default='./checkpoints/', help='directory to save the model file')
+    parser.add_argument('--save_on_train_epoch_end', type=bool, default=True,
+                        help='Whether to run checkpointing at the end of the training epoch')
+    parser.add_argument('--filename', type=str, default='clip-rsicd-{epoch:02d}-{val_loss:.2f}',
+                        help='checkpoint filename. Can contain named formatting options to be auto-filled')
+    parser.add_argument('--save_top_k', type=int, default=5,
+                        help='the best k models according to the quantity monitored will be saved')
+
+    # used both in model and datamodule
+    parser.add_argument('--batch_size', type=int, default=128, help='size of the batch')
+
+    # used both for early stopping and checkpointing
+    parser.add_argument('--mode', type=str, default='max',
+                        help='number of checks with no improvement after which training will be stopped')
+    parser.add_argument('--monitor_metric', type=str, default='val_loss', help='metric to monitor for early stopping '
+                                                                               'and checkpointing')
 
     args = parser.parse_args()
 
