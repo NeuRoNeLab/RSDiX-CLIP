90cf492eb75a0f2cd3c512c592648a6215fa8ef5
Angelo Nazzaro <angelonazzaro12@gmail.com>
2023-06-19 11:38:45 +0200

    add train and split constants to datasets

--- datasets/nais.py
+++ datasets/nais.py
@@ -1,14 +1,15 @@
 from .captioningDataset import CaptioningDataModule
+from utils import TRAIN_SPLIT_PERCENTAGE, VAL_SPLIT_PERCENTAGE
 
 
 class NAIS(CaptioningDataModule):
     """ NAIS Dataset. """
 
     def __init__(self, annotations_file: str, img_dir: str, img_transform=None, target_transform=None,
-                 train_split_percentage: float = 80, val_split_percentage: float = 10, batch_size: int = 32,
-                 num_workers: int = 0, shuffle: bool = False):
+                 train_split_percentage: float = TRAIN_SPLIT_PERCENTAGE,
+                 val_split_percentage: float = VAL_SPLIT_PERCENTAGE, batch_size: int = 512, num_workers: int = 0,
+                 shuffle: bool = False):
         super().__init__(annotations_file=annotations_file, img_dir=img_dir, img_transform=img_transform,
                          target_transform=target_transform, train_split_percentage=train_split_percentage,
                          val_split_percentage=val_split_percentage, batch_size=batch_size, num_workers=num_workers,
                          shuffle=shuffle)
-
