1b0ca28a9fdbfeb56e325e6420f5a947e58b58b8
Angelo Nazzaro <angelonazzaro12@gmail.com>
2023-07-25 21:45:21 +0200

    refactorized CLIPWrapper to match fb-OTTER's EMA, SINKHORN and DISTILLATION LOSS implementations

--- models/model_utils.py
+++ models/model_utils.py
@@ -1,17 +1,17 @@
-import math
-from typing import final
-
 import torch
-import torch.nn.functional as f
+
+from typing import final
 from sentence_transformers.util import cos_sim
 
 REDUCTIONS: final = frozenset(["mean", "average", "avg", "sum", "add", "none"])
 
 
+# Source: https://github.com/facebookresearch/OTTER/blob/main/models/model_util.py
 def _has_nan_or_inf(x):
     return torch.isnan(x).any() or torch.isinf(x).any()
 
 
+# Source: https://github.com/facebookresearch/OTTER/blob/main/models/model_util.py
 @torch.no_grad()
 def sinkhorn(cost_mat, eps=0.05, niter=5, r_prob=None, c_prob=None):
     """
@@ -19,101 +19,68 @@ def sinkhorn(cost_mat, eps=0.05, niter=5, r_prob=None, c_prob=None):
     r_prob: s1, s2, ..., sn, M
     c_prob: s1, s2, ..., sn, N
     """
-    Q = torch.exp(-cost_mat / eps)
-    Q = Q / Q.sum(dim=[-2, -1], keepdim=True)
-    M, N = Q.shape[-2], Q.shape[-1]
+
+    q = torch.exp(-cost_mat / eps)
+    q = q / q.sum(dim=[-2, -1], keepdim=True)
+    m, n = q.shape[-2], q.shape[-1]
 
     if r_prob is not None:
         # s1, ..., sn, M -> s1, ..., sn, M, 1
         r_prob = (r_prob / r_prob.sum(dim=-1, keepdim=True)).unsqueeze(-1)
         assert not _has_nan_or_inf(r_prob)
     else:
-        r_prob = 1 / M
+        r_prob = 1 / m
 
     if c_prob is not None:
         # s1, ..., sn, N -> s1, ..., sn, 1, N
         c_prob = (c_prob / c_prob.sum(dim=-1, keepdim=True)).unsqueeze(-2)
         assert not _has_nan_or_inf(c_prob)
     else:
-        c_prob = 1 / N
+        c_prob = 1 / n
 
     for _ in range(niter):
         # normalize each row: total weight per row must be r_prob
-        Q /= Q.sum(dim=-1, keepdim=True)
-        Q *= r_prob
+        q /= q.sum(dim=-1, keepdim=True)
+        q *= r_prob
         # normalize each column: total weight per column must be c_prob
-        Q /= Q.sum(dim=-2, keepdim=True)
-        Q *= c_prob
-    return Q
-
-
-# @torch.no_grad()
-# Source: https://github.com/facebookresearch/swav/blob/5e073db0cc69dea22aa75e92bfdd75011e888f28/main_swav.py#L354
-# def sinkhorn(out):
-#     q = torch.exp(out / 0.05).t()  # q is k-by-b for consistency with notations from our paper
-#     b = q.shape[1]  # number of samples to assign
-#     k = q.shape[0]  # how many prototypes
-#
-#     # make the matrix sums to 1
-#     sum_q = torch.sum(q)
-#     q /= sum_q
-#
-#     for it in range(3):
-#         # normalize each row: total weight per prototype must be 1/k
-#         sum_of_rows = torch.sum(q, dim=1, keepdim=True)
-#         q /= sum_of_rows
-#         q /= k
-#
-#         # normalize each column: total weight per sample must be 1/b
-#         q /= torch.sum(q, dim=0, keepdim=True)
-#         q /= b
-#
-#     q *= b  # the columns must sum to 1 so that q is an assignment
-#     return q.t()
+        q /= q.sum(dim=-2, keepdim=True)
+        q *= c_prob
+    return q / q.sum(dim=1, keepdim=True)
 
 
 @torch.no_grad()
 def compute_similarities(i_emb, t_emb):
-    sim_ii, sim_tt = i_emb @ i_emb.t(), t_emb @ t_emb.t()
-    sim_it, sim_ti = i_emb @ t_emb.t(), t_emb @ i_emb.t()
+    sim_ii, sim_tt = torch.matmul(i_emb, i_emb.t()), torch.matmul(t_emb, t_emb.t())
+    sim_it, sim_ti = torch.matmul(i_emb, t_emb.t()), torch.matmul(t_emb, i_emb.t())
     return sim_ii, sim_tt, sim_it, sim_ti
 
 
 @torch.no_grad()
-def compute_teacher_targets(teacher_image_embs, teacher_captions_embs):
-    sim_ii, sim_tt, sim_it, sim_ti = compute_similarities(torch.cat(teacher_image_embs),
-                                                          torch.cat(teacher_captions_embs))
+def compute_teacher_targets(teacher_images_embs, teacher_text_embs, ii_coeff, tt_coeff, sinkhorn_lambda, sinkhorn_iter,
+                            remove_diag):
+    sim_ii, sim_tt, sim_it, sim_ti = compute_similarities(teacher_images_embs, teacher_text_embs)
 
-    diag = (torch.eye(*sim_ii.shape) * 0 * 1e2).to(teacher_image_embs[0].device)
-    sim_ii = (sim_ii - diag) * 1.0
-    sim_tt = (sim_tt - diag) * 1.0
-    # optimal transport
+    diag = (torch.eye(*sim_ii.shape) * remove_diag * 1e2).to(teacher_images_embs.device)
+    sim_ii = (sim_ii - diag) * ii_coeff
+    sim_tt = (sim_tt - diag) * tt_coeff
+
+    # Optimal transport
     # Perform sinkhorn based on the cost matrix, and then row-normalize
     # to get target probability.
-    img_cost = - (sim_ii + sim_tt + sim_it)
-    caption_cost = - (sim_ii + sim_tt + sim_ti)
-    img_target, caption_target = sinkhorn(img_cost), sinkhorn(caption_cost)
+    images_cost_mat = - (sim_ii + sim_tt + sim_it)
+    text_cost_mat = - (sim_ii + sim_tt + sim_ti)
 
-    img_target /= img_target.sum(dim=1, keepdim=True)
-    caption_target /= caption_target.sum(dim=1, keepdim=True)
+    images_target_prob = sinkhorn(images_cost_mat, sinkhorn_lambda, sinkhorn_iter)
+    text_target_prob = sinkhorn(text_cost_mat, sinkhorn_lambda, sinkhorn_iter)
 
-    return img_target, caption_target
+    images_target_prob /= images_target_prob.sum(dim=1, keepdim=True)
+    text_target_prob /= text_target_prob.sum(dim=1, keepdim=True)
+
+    return images_target_prob, text_target_prob
 
 
 @torch.no_grad()
 def compute_st_similarities(clip_image_embeddings, clip_text_embeddings, st_embeddings):
-    if isinstance(clip_image_embeddings, list):
-        if len(clip_image_embeddings) > 1:
-            clip_image_embeddings = torch.stack(clip_image_embeddings)
-        else:
-            clip_image_embeddings = clip_image_embeddings[0]
-
-    if isinstance(clip_text_embeddings, list):
-        if len(clip_text_embeddings) > 1:
-            clip_text_embeddings = torch.stack(clip_text_embeddings)
-        else:
-            clip_text_embeddings = clip_text_embeddings[0]
-
     image_image_similarities = cos_sim(clip_image_embeddings, clip_image_embeddings)
     image_text_similarities = cos_sim(clip_image_embeddings, clip_text_embeddings)
     text_text_similarities_clip = cos_sim(clip_text_embeddings, clip_text_embeddings)
@@ -148,36 +115,21 @@ def compute_mse_similarities(image_image_similarities: torch.Tensor,
         return mse_tensor
 
 
-def compute_losses(image_embs, caption_embs, scale, ground_truth, img_target, caption_target, sink_temp, kl_coeff,
-                   reduction="batchmean"):
-    logits_unscaled = torch.cat(image_embs) @ torch.cat(caption_embs).t()
-    scale = scale.exp()
-    sink_temp = sink_temp.exp()
-    logits = logits_unscaled * scale
+@torch.no_grad()
+def compute_mse(clip_image_embeddings, clip_text_embeddings, st_embeddings, device):
+    ii_sim, it_sim, tt_sim_clip, tt_sim_st = compute_st_similarities(clip_image_embeddings, clip_text_embeddings,
+                                                                     st_embeddings)
 
-    contrastive_loss = (f.cross_entropy(logits, ground_truth) + f.cross_entropy(logits.t(), ground_truth)) / 2
-    distillation_loss = (f.kl_div(f.log_softmax(logits_unscaled * sink_temp, dim=-1), img_target, reduction=reduction) +
-                         f.kl_div(f.log_softmax(logits_unscaled.t() * sink_temp, dim=-1), caption_target,
-                                  reduction=reduction)) / 2 * kl_coeff
+    if tt_sim_st.device != device:
+        tt_sim_st = tt_sim_st.to(device)
 
-    return contrastive_loss, distillation_loss
+    return compute_mse_similarities(ii_sim, it_sim, tt_sim_clip, tt_sim_st)
 
 
-def get_image_caption_chunks(image, caption, batch_size):
-    n = math.ceil(len(image) // batch_size)
-    image_chunks = torch.chunk(image, n)
-    caption_chunks_ids = torch.chunk(torch.arange(len(image)), n)
+def compute_accuracy(images_logits: torch.Tensor, batch_size: int):
+    ground_truth = torch.arange(len(images_logits)).to(images_logits.device)
 
-    # getting the caption for the current gpu
-    # then stacking them to match image_chunks' type
-    caption_chunks = []
-    caption = list(caption)
-    for s in caption_chunks_ids[0]:
-        caption_chunks.append(caption[s.item()])
-    caption_chunks = (torch.stack(caption_chunks),)
+    acc_i = (torch.argmax(images_logits, 1) == ground_truth).sum()
+    acc_t = (torch.argmax(images_logits, 0) == ground_truth).sum()
 
-    return image_chunks, caption_chunks
-
-
-def ema(s, t):
-    return s * (1 - 0.999) + t * 0.999
+    return (acc_i + acc_t) / 2 / batch_size
--- models/wrapper.py
+++ models/wrapper.py
@@ -1,53 +1,57 @@
-import copy
-
 import lightning as l
 import torch
-import torch.nn.functional as f
 import yaml
-from cosine_annealing_warmup import CosineAnnealingWarmupRestarts
-from sentence_transformers import SentenceTransformer
-from torch.optim.lr_scheduler import LinearLR
+import copy
+
 from transformers import CLIPModel
+from typing import Optional
 
 from loss import DistillationLoss
-from utils import CONFIG_DIR, VIT_CONFIG_FILE, MINIBATCH_SIZE, BATCH_SIZE, IMAGE_FIELD, CAPTION_FIELD, BETAS, \
+from sentence_transformers import SentenceTransformer
+from cosine_annealing_warmup import CosineAnnealingWarmupRestarts
+from torch.optim.lr_scheduler import LinearLR
+
+from utils import CONFIG_DIR, VIT_CONFIG_FILE, IMAGE_FIELD, CAPTION_FIELD, BETAS, \
     RAW_FIELD_CAPTION
+from .model_utils import compute_mse, compute_accuracy, compute_teacher_targets
 from .ema import ExponentialMovingAverage
-from .model_utils import ema, compute_st_similarities, compute_mse_similarities, \
-    compute_teacher_targets
 
 
 class CLIPWrapper(l.LightningModule):
-    st_model = SentenceTransformer("all-MiniLM-L6-v2")
+    """
+    A Pytorch-Lightning based wrapper for Hugging Face's CLIP implementation.
+    """
 
-    def __init__(self, model: str = "openai/clip-vit-base-patch32", batch_size: int = MINIBATCH_SIZE,
-                 kl_coeff: float = 1.0, lr: float = None, use_warmup: str = "cosine", warmup_steps: int = 0,
-                 betas: tuple[float, float] = BETAS, alpha: float = 0.5, eps: float = 1e-08, weight_decay: float = 0.2,
-                 start_factor: float = 0.3333333333333333, end_factor: float = 1.0, total_iters: int = 5):
+    _st_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    def __init__(self, model: str = "openai/clip-vit-base-patch32",
+                 lr: Optional[float] = None, alpha: float = 0.5, ema_decay: float = 0.999,
+                 weight_decay: float = 0.1, start_factor: float = 0.3333333333333333,
+                 end_factor: float = 1.0, total_iters: int = 5, use_warmup: str = "cosine",
+                 warmup_steps: int = 0, eps: float = 1e-08,
+                 betas: tuple[float, float] = BETAS,
+                 sinkhorn_lambda: float = 0.1, sinkhorn_iter: int = 4, ii_coeff: float = 1.0, tt_coeff: float = 1.0,
+                 remove_diag: bool = False):
         super().__init__()
 
         if use_warmup != "cosine" and use_warmup != "linear":
-            raise Exception(f"{use_warmup} not supported. Try 'cosine' or 'linear'. ")
+            raise ValueError(f"{use_warmup} not supported. Try 'cosine' or 'linear'.")
 
+        # Init main model
         self._student = CLIPModel.from_pretrained(model)
 
+        # If no LR is passed, the default one from the model's configuration will be used
+        # Note: Only Visual Transformers are currently supported
         if lr is None:
-            model_type = "B" if "base" in model else "L"
             model_size = model.split("patch", 1)[1]
-            model_name = f"ViT-{model_type}/{model_size}"
+            model_name = f"Vit-B/{model_size}"
 
-            with open(f"{CONFIG_DIR}/{VIT_CONFIG_FILE}") as stream:
+            with open(f"f{CONFIG_DIR}/{VIT_CONFIG_FILE}") as stream:
                 config = yaml.safe_load(stream)[model_name]
 
             self._lr = float(config["learning_rate"])
         else:
             self._lr = lr
-
-        # changed from minibatch_size to batch_size to match Lightning's BatchSizeFinder expectations
-        if batch_size == MINIBATCH_SIZE:
-            self._batch_size = BATCH_SIZE
-        else:
-            self._batch_size = batch_size
 
         self._use_warmup = use_warmup
         self._weight_decay = weight_decay
@@ -55,158 +59,123 @@ class CLIPWrapper(l.LightningModule):
         self._alpha = alpha
         self._eps = eps
 
-        self.ema_model = ExponentialMovingAverage(
-            self.student.parameters(), decay=0.999,
-        )
-
         if use_warmup == "cosine":
             self._warmup_steps = warmup_steps
         else:
             self._start_factor = start_factor
             self._end_factor = end_factor
             self._total_iters = total_iters
-        # Source: https://lightning.ai/docs/pytorch/stable/accelerators/accelerator_prepare.html
-        self.register_buffer("_sink_temp", torch.nn.Parameter(torch.ones([]) * self._student.logit_scale.item()))
 
-        # init self-distillation model
-        self.dist_loss = DistillationLoss()
+        # Init self-distillation loss and teacher model
+        self._dist_loss = DistillationLoss()
         self._teacher = copy.deepcopy(self._student)
+        self._ema_model = ExponentialMovingAverage(self._student.parameters(), decay=ema_decay)
 
-        self._kl_coeff = kl_coeff
+        self._sinkhorn_lambda = sinkhorn_lambda
+        self._sinkhorn_iter = sinkhorn_iter
+        self._ii_coeff = ii_coeff
+        self._tt_coeff = tt_coeff
+        self._remove_diag = float(remove_diag)
 
-        # save hyperparameters when checkpointing
+        # Save hyperparameters when checkpointing
         self.save_hyperparameters()
 
-    # necessary to use Lightning's LearningRateFinder
-    @property
-    def lr(self):
-        return self._lr
+    def get_embeddings(self, images, text, teacher: bool = False):
+        # Get the embeddings
+        images_embeds = self.encode_image(images=images, teacher=teacher)
+        text_embeds = self.encode_text(text=text, teacher=teacher)
 
-    @lr.setter
-    def lr(self, lr):
-        self._lr = lr
-
-    def get_embeddings(self, images, captions, teacher=False):
-        images_embeds = self.encode_image(image=images, teacher=teacher)
-        captions_embeds = self.encode_text(caption=captions, teacher=teacher)
-
-        # normalized features
+        # Normalize them
         images_embeds = images_embeds / images_embeds.norm(p=2, dim=-1, keepdim=True)
-        captions_embeds = captions_embeds / captions_embeds.norm(p=2, dim=-1, keepdim=True)
+        text_embeds = text_embeds / text_embeds.norm(p=2, dim=-1, keepdim=True)
 
-        return images_embeds, captions_embeds
+        return images_embeds, text_embeds
 
-    # Training loss: https://github.com/openai/CLIP/issues/83
-    # Multi-GPU support: https://github.com/MicPie/clasp
-    def training_step(self, batch, batch_idx):
-        # get optimizers and lr scheduler
-        optimizer = self.optimizers()
+    def training_step(self, batch, batch_idx) -> torch.Tensor:
+        images, text, raw_text = batch[IMAGE_FIELD], batch[CAPTION_FIELD], batch.pop(RAW_FIELD_CAPTION)
 
-        images, captions, raw_captions = batch[IMAGE_FIELD], batch[CAPTION_FIELD], batch[RAW_FIELD_CAPTION]
+        # Update the teacher model
+        self.update_teacher()
 
-        del batch[RAW_FIELD_CAPTION]
+        # Get student embeddings and compute unscaled logits
+        student_images_embeds, student_text_embeds = self.get_embeddings(images=images, text=text)
+        unscaled_student_images_logits = torch.matmul(student_images_embeds, student_text_embeds.t())
 
-        # calculate original statistics
+        # Compute teacher embeddings and self-distillation targets
         with torch.no_grad():
-            teacher_image_embs, teacher_captions_embs = self.get_embeddings(images, captions, teacher=True)
-            img_target, caption_target = compute_teacher_targets([teacher_image_embs], [teacher_captions_embs])
+            teacher_images_embeds, teacher_text_embeds = self.get_embeddings(images=images, text=text, teacher=True)
+            teacher_images_embeds = teacher_images_embeds.detach()
+            teacher_text_embeds = teacher_text_embeds.detach()
+            images_target_prob, text_target_prob = compute_teacher_targets(teacher_images_embeds, teacher_text_embeds,
+                                                                           self._ii_coeff, self._tt_coeff,
+                                                                           self._sinkhorn_lambda, self._sinkhorn_iter,
+                                                                           self._remove_diag)
+        # Compute self-distillation loss
+        images_dist_loss = self._dist_loss(pred=unscaled_student_images_logits, target_prob=images_target_prob)
+        text_dist_loss = self._dist_loss(pred=unscaled_student_images_logits.t(), target_prob=text_target_prob)
+        distillation_loss = (images_dist_loss + text_dist_loss) / 2
 
-        if isinstance(optimizer, list):
-            optimizer = optimizer[0]
-        optimizer.zero_grad()
-
+        # Compute contrastive loss
         contrastive_loss = self.forward(batch).loss
 
-        student_images_embs, student_caption_embs = self.get_embeddings(images, captions)
-        student_image_logits_unscaled = torch.cat([student_images_embs]) @ torch.cat([student_caption_embs]).t()
-        sink_temp = self._sink_temp.exp()
+        # Total loss
+        loss = (self._alpha * contrastive_loss) + ((1 - self._alpha) * distillation_loss)
 
-        # contrastive loss ground truth -> Identity matrix
-        ground_truth = torch.arange(len(student_image_logits_unscaled)).long().to(student_image_logits_unscaled.device)
-        # distillation_loss = (f.kl_div(f.log_softmax(student_image_logits_unscaled * sink_temp, dim=-1),
-        #                               img_target,
-        #                               reduction="batchmean") +
-        #                      f.kl_div(f.log_softmax(student_image_logits_unscaled.t() * sink_temp, dim=-1),
-        #                               caption_target,
-        #                               reduction="batchmean")) / 2 * self._kl_coeff
-        img_dist_loss = self.dist_loss(pred=student_image_logits_unscaled, target_prob=img_target)
-        txt_dist_loss = self.dist_loss(pred=student_image_logits_unscaled.t(), target_prob=caption_target)
+        # Compute training metrics
+        # CLIP/Sentence-BERT MSE in embeddings similarities
+        mse = compute_mse(clip_image_embeddings=student_images_embeds, clip_text_embeddings=student_text_embeds,
+                          st_embeddings=self._st_model.encode(raw_text), device=images.device)
 
-        distillation_loss = img_dist_loss + txt_dist_loss
+        # Compute accuracy (contrastive loss ground truth = identity matrix)
+        accuracy = compute_accuracy(images_logits=unscaled_student_images_logits, batch_size=len(images))
 
-        img_img_sim, img_txt_sim, txt_txt_sim_clip, txt_txt_sim_st \
-            = compute_st_similarities(student_images_embs, student_caption_embs,
-                                      self.st_model.encode(raw_captions, device=batch[IMAGE_FIELD].device))
-
-        if txt_txt_sim_st.device != batch[IMAGE_FIELD].device:
-            txt_txt_sim_st = txt_txt_sim_st.to(batch[IMAGE_FIELD].device)
-
-        acc_i = (torch.argmax(student_image_logits_unscaled, 1) == ground_truth).sum()
-        acc_t = (torch.argmax(student_image_logits_unscaled, 0) == ground_truth).sum()
-
-        loss = self._alpha * contrastive_loss + (1 - self._alpha) * distillation_loss
-
+        # Log metrics
         self.log_dict({'loss': loss.item(),
-                       'mse_sentence_transformer': compute_mse_similarities(img_img_sim, img_txt_sim, txt_txt_sim_clip,
-                                                                            txt_txt_sim_st),
-                       'acc': (acc_i + acc_t) / 2 / len(images)},
+                       'mse_sentence_transformer': mse,
+                       'acc': accuracy},
                       prog_bar=True, on_step=True, on_epoch=True, logger=True, enable_graph=True)
-
-        self.ema_model.update(self._student.parameters())
-        self.ema_model.copy_to(self._teacher.parameters())
 
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        images, captions, raw_captions = batch[IMAGE_FIELD], batch[CAPTION_FIELD], batch[RAW_FIELD_CAPTION]
-        image_embs, caption_embs = self.get_embeddings(images, captions)
-
-        del batch[RAW_FIELD_CAPTION]
+    def validation_step(self, batch, batch_idx) -> torch.Tensor:
+        images, text, raw_text = batch[IMAGE_FIELD], batch[CAPTION_FIELD], batch.pop(RAW_FIELD_CAPTION)
+        images_embeds, text_embeds = self.get_embeddings(images=images, text=text)
 
         outputs = self.forward(batch)
-        image_logits, caption_logits = outputs.logits_per_image, outputs.logits_per_text
-        ground_truth = torch.arange(len(image_logits), device=batch[IMAGE_FIELD].device)
 
-        acc_i = (torch.argmax(image_logits, 1) == ground_truth).sum()
-        acc_t = (torch.argmax(caption_logits, 1) == ground_truth).sum()
-
-        img_img_sim, img_txt_sim, txt_txt_sim_clip, txt_txt_sim_st \
-            = compute_st_similarities(image_embs, caption_embs,
-                                      self.st_model.encode(raw_captions, device=batch[IMAGE_FIELD].device))
-
-        if txt_txt_sim_st.device != batch[IMAGE_FIELD].device:
-            txt_txt_sim_st = txt_txt_sim_st.to(batch[IMAGE_FIELD].device)
+        accuracy = compute_accuracy(images_logits=outputs.logits_per_image, batch_size=len(images_embeds))
+        mse = compute_mse(clip_image_embeddings=images_embeds, clip_text_embeddings=text_embeds,
+                          st_embeddings=self._st_model.encode(raw_text), device=images.device)
 
         self.log_dict({'val_loss': outputs.loss.item(),
-                       'val_mse_sentence_transformer': compute_mse_similarities(img_img_sim, img_txt_sim,
-                                                                                txt_txt_sim_clip, txt_txt_sim_st),
-                       'val_acc': (acc_i + acc_t) / 2 / len(image_logits)},
+                       'val_mse_sentence_transformer': mse,
+                       'val_acc': accuracy},
                       prog_bar=True, on_step=True, on_epoch=True, logger=True, enable_graph=True)
 
-    def encode_image(self, image, teacher=False):
-        # image = inputs["pixel_values"]
-        return self._student.get_image_features(image) if teacher is False else \
-            self._teacher.get_image_features(image)
+        return outputs.loss
 
-    def encode_text(self, caption, teacher=False):
-        # caption = inputs["input_ids"]
-        return self._student.get_text_features(caption) if teacher is False else \
-            self._teacher.get_text_features(caption)
+    def encode_image(self, images: torch.Tensor, teacher: bool = False) -> torch.FloatTensor:
+        return self._student.get_image_features(images) if teacher is False else \
+            self._teacher.get_image_features(images)
+
+    def encode_text(self, text: torch.Tensor, teacher: bool = False) -> torch.FloatTensor:
+        return self._student.get_text_features(text) if teacher is False else \
+            self._teacher.get_text_features(text)
 
     def forward(self, inputs, return_loss: bool = True):
         return self._student(**inputs, return_loss=return_loss)
 
     def update_teacher(self):
-        for teacher, student in zip(self._teacher.parameters(), self._student.parameters()):
-            teacher.data.copy_(ema(student.data, teacher.data))
+        self._ema_model.update(self._student.parameters())
+        self._ema_model.copy_to(self._teacher.parameters())
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> dict:
         optimizer = torch.optim.AdamW(params=self._student.parameters(), lr=self._lr, eps=self._eps,
                                       betas=self._betas, weight_decay=self._weight_decay)
 
-        # Source: https://github.com/PyTorchLightning/pytorch-lightning/issues/5449
-        # Source: https://github.com/openai/CLIP/issues/107
         if self._use_warmup == "cosine":
+            # Source: https://github.com/PyTorchLightning/pytorch-lightning/issues/5449
+            # Source: https://github.com/openai/CLIP/issues/107
             lr_scheduler = CosineAnnealingWarmupRestarts(optimizer=optimizer,
                                                          first_cycle_steps=self.trainer.estimated_stepping_batches,
                                                          max_lr=self._lr,
