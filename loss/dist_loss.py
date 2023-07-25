45bf503af8a559bc2f3b25ef8f54465a9e96580a
Angelo Nazzaro <angelonazzaro12@gmail.com>
2023-07-25 21:45:39 +0200

    update dist_loss.py

--- loss/dist_loss.py
+++ loss/dist_loss.py
@@ -1,8 +1,3 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 # Source: https://github.com/facebookresearch/OTTER/blob/main/loss/kl_div_loss.py#L10
 
 import torch
